a4b11391d81ce0d7e6dc2c9093dedbabd1c94d2b
ashleygoolam <12goolamag@gmail.com>
2023-04-06 12:55:25 +0800

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Get: /HelloWorld
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Get: /HelloWorld/Welcome?name=...&number=
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["message"] = "Hello, " + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+    }
+}
